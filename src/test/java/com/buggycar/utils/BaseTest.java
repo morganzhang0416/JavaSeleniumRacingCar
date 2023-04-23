f31ac5f43b734f3742d30adf3a88a67051934e06
morganzhang0416 <morgan.zhang0416@gmail.com>
2023-04-23 20:52:03 +1200

    update yml file

--- src/test/java/com/buggycar/utils/BaseTest.java
+++ src/test/java/com/buggycar/utils/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 // import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvBuilder;
 
 // setup and quite browser in each test case
 public class BaseTest {
@@ -14,7 +16,10 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         // Set path to the chromedriver executable
-        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+        DotenvBuilder dotenvBuilder = Dotenv.configure();
+        Dotenv dotenv = dotenvBuilder.load();
+        String driverpath = dotenv.get("CHROMEDRIVER_PATH");
+        System.setProperty("webdriver.chrome.driver", driverpath);
 
         // Create a new ChromeDriver instance
         driver = new ChromeDriver();
